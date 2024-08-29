d15557804f7bde50f89830ce357a7c032fda2003
Ruhul Amin <ruhulamin.et15@gmail.com>
2024-08-29 16:35:49 +0600

    update origin

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,10 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://x-mega-pro-jibon-roy-jibon-roys-projects.vercel.app/login",
+    ],
     credentials: true,
   })
 );
