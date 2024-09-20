e723c08bb041d21d9bffb579e160dda17faa6970
Ruhul Amin <ruhulamin.et15@gmail.com>
2024-09-20 12:35:05 +0600

    update aws

--- src/app/middleware/fileUpload.ts
+++ src/app/middleware/fileUpload.ts
@@ -1,13 +1,21 @@
-import AWS from "aws-sdk";
+/* eslint-disable no-useless-catch */
+import {
+  S3Client,
+  PutObjectCommand,
+  PutObjectCommandInput,
+} from "@aws-sdk/client-s3";
 import config from "../config";
 import fs from "fs";
+import { URL } from "url";
 
 // Configure AWS SDK for DigitalOcean Spaces
-const spacesEndpoint = new AWS.Endpoint(config.doSpacesEndPoint as string);
-const s3 = new AWS.S3({
-  endpoint: spacesEndpoint,
-  accessKeyId: config.doAccessKey,
-  secretAccessKey: config.doSecretKey,
+const s3 = new S3Client({
+  endpoint: config.doSpacesEndPoint as string,
+  region: "us-east-1", // Set a region, DigitalOcean Spaces requires it
+  credentials: {
+    accessKeyId: config.doAccessKey as string,
+    secretAccessKey: config.doSecretKey as string,
+  },
 });
 
 const uploadFileAndGetLink = async (
@@ -21,7 +29,7 @@ const uploadFileAndGetLink = async (
     // Read the file content from the file path
     const fileContent = fs.readFileSync(filePath);
 
-    const params: AWS.S3.PutObjectRequest = {
+    const params: PutObjectCommandInput = {
       Bucket: config.doBucketName as string,
       Key: `songs/${fileName}`, // Path to store in the S3 bucket
       Body: fileContent,
@@ -29,15 +37,16 @@ const uploadFileAndGetLink = async (
     };
 
     // Uploading file to DigitalOcean Space (S3-compatible)
-    const data = await s3.upload(params).promise();
+    const command = new PutObjectCommand(params);
+    await s3.send(command);
 
-    console.log(`File uploaded successfully: ${data.Location}`);
-    return data.Location; // Return the public URL of the file
+    // Construct the public URL for the uploaded file
+    const url = new URL(`songs/${fileName}`, config.doSpacesEndPoint);
+    return url.href; // Return the public URL of the file
   } catch (err) {
-    console.error("Error uploading song:", err);
+    // console.error("Error uploading song:", err);
     throw err; // Ensure errors are caught by the caller
   }
 };
-
 
 export default uploadFileAndGetLink;
